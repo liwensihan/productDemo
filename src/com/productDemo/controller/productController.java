aa69cdc7ae5dbbc2fb261833b1451a0e0a84449a
whiteandnight <741345327@qq.com>
2018-04-12 11:57:29 +0800

    加了一个李文思汗

--- src/com/productDemo/controller/productController.java
+++ src/com/productDemo/controller/productController.java
@@ -35,6 +35,7 @@ public class productController {
     @RequestMapping("/index")
     public ModelAndView index(ModelAndView modelAndView ,Integer pageNo){
         System.out.println("我是王健");
+        System.out.println("我是李文思汗");
         Pager pager = new Pager();
         pager.setTotalCount(productService.getCount());
         if (pageNo==null){
